13c7e8ecb6a8920254b911c745413ba4d9dc65ff
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2015-11-27 12:21:11 -0300

    Se agrega account para manejar las cuentas de usuario. closed #4

---
+++ app/models/account.rb
@@ -0,0 +1,9 @@
+class Account < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_one :profile
+  has_many :budgets
+end
